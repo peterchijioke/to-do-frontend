730b0445a4b6ff2d408c52d0da17729711f8bdd7
Chukwu (Chijioke)  Peter <peterchijioke1@gmail.com>
2024-11-23 13:28:33 +0100

    fix: persisted the user data

--- src/providers/user.provider.ts
+++ src/providers/user.provider.ts
@@ -1,5 +1,5 @@
 import { create } from 'zustand';
-import { Task, User } from '../types/task.type';
+import {  User } from '../types/task.type';
 import { persist } from 'zustand/middleware';
 
 interface TaskStoreProp {
