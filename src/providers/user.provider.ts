9cc7573516e796ff9a089c3dc8e2c3248afe9635
Chukwu (Chijioke)  Peter <peterchijioke1@gmail.com>
2024-11-23 13:28:21 +0100

    fix: persisted the user data

--- src/providers/user.provider.ts
+++ src/providers/user.provider.ts
@@ -1,14 +1,19 @@
 import { create } from 'zustand';
 import { Task, User } from '../types/task.type';
+import { persist } from 'zustand/middleware';
 
 interface TaskStoreProp {
   user: User | null;
   setUser: (user: User | null) => void;
 }
 
-const useUserStore = create<TaskStoreProp>((set) => ({
+const useUserStore = create<TaskStoreProp>()(persist(
+  (set) => ({
   user: null, 
   setUser: (user) => set(() => ({ user })),
+})
+  ,{
+name:'user-storage'
 }));
 
 export default useUserStore;
